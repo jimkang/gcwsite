b40ede342b5ab1f4134511d37d1649deee22ce22
Jim Kang <jimkang@gmail.com>
2011-09-30 00:14:22 -0400

    Added photo-container to overlay, plus functions and styles to make it work as a basic slideshow that advances when clicked.

--- js/photo_gallery.js
+++ js/photo_gallery.js
@@ -1,5 +1,38 @@
 // Depends on utlities.js.
 
+var gPhotos = 
+[
+	{filename: "jotsview_screenshot.png", width: 320, height: 480},
+	{filename: "editorviewlandscape_screenshot.png", width: 480, height: 320},
+	{filename: "outletselection_screenshot.png", width: 320, height: 480},
+	{filename: "imageeditor_screenshot.png", width: 320, height: 480},
+	{filename: "jotsviewhelp_screenshot.png", width: 320, height: 480},
+	{filename: "dataliberation_screenshot.png", width: 320, height: 480},
+	{filename: "search_screenshot.png",width: 320, height: 480}
+];
+
+var gPhotoIndex = 0;
+var photoCount = 7;
+
+function incrementPhotoIndex()
+{
+	gPhotoIndex++;
+	if (gPhotoIndex >= photoCount)
+	{
+		gPhotoIndex = 0;
+	}
+}
+
+function decrementPhotoIndex()
+{
+	gPhotoIndex--;
+	if (gPhotoIndex < 0)
+	{
+		gPhotoIndex = photoCount - 1;
+	}	
+}
+
+
 function showOverlay()
 {
   var overlay = document.getElementById('overlay');
@@ -7,7 +40,62 @@ function showOverlay()
   // The "setTimeout" ensures that the initial style is rendered, and hence allows the transition to run.
   window.setTimeout(function() {
     overlay.addClassName('visible');
+	showNextPhoto();
   }, 0);
+}
+
+function hideOverlay()
+{
+  var overlay = document.getElementById('overlay');
+  overlay.removeClassName('visible');
+  // The "setTimeout" ensures that the initial style is rendered, and hence allows the transition to run.
+  window.setTimeout(function() {
+    overlay.addClassName('hidden');
+  }, 0);
+}
+
+// function overlayIsVisible()
+// {
+// 	var overlay = document.getElementById('overlay');
+// 	return overlay.hasClassName('visible');
+// }
+
+function showNextPhoto()
+{		
+  	var photoContainer = document.getElementById('photo-container');
+  	photoContainer.removeAllChildren();
+
+ 	var newContent = createElementForAssetAtIndex(gPhotoIndex);
+	incrementPhotoIndex();
+	
+  	photoContainer.appendChild(newContent);	
+}
+
+function createElementForAssetAtIndex(assetIndex)
+{
+	if (assetIndex < 0 || assetIndex >= photoCount)
+	{
+ 		return undefined;
+	
+	}
+	var container = document.createElement('div');
+	container.className = 'contents';
+
+	var photoData = gPhotos[assetIndex];
+	var mediaElement;
+	if (photoData.filename.match(/[.jpg|.png|.gif]$/)) 
+	{
+    	// If the asset has an image extension, make an image element.
+    	mediaElement = new Image();
+    	// Register a load listener; when it fires, our handleEvent() method will get called.
+    	mediaElement.addEventListener('load', this, false);
+    	mediaElement.src = "img/" + photoData.filename;
+		mediaElement.width = photoData.width;
+		mediaElement.height = photoData.height;
+	} 
+
+	container.appendChild(mediaElement);
+	return container;
 }
 
 function setUpTouchables()
@@ -20,20 +108,30 @@ function setUpTouchables()
     // hook up an event listener with 'this' as the target, inside a loop.
     	curElement.addEventListener('click', triggerClicked.bind(this, curElement), false);
 	}
+	
+	var overlay = document.getElementById('overlay');
+	overlay.addEventListener('click', hideOverlay.bind(this, overlay), false);
+	
+	$('#photo-container').bind('click', photoContainerClicked);
+	//   	var photoContainer = document.getElementById('photo-container');
+	// photoContainer.addEventListener('click', showNextPhoto.bind(this, photoContainer), false);
+	
 }
 
-var gGallery;
 function triggerClicked()
-{
-  if (gGallery)
-    return;
-
+{	
   // var galleryContainer = document.getElementById('gallery-container');
   // gGallery = new Gallery(galleryContainer);
 
-  showOverlay();
+	showOverlay();
 }
 
+function photoContainerClicked(event)
+{
+	// event.stopPropagation();
+	showNextPhoto();
+	return false; // Equivalent to event.stopPropagation.
+}
 
 // The preferred way to call a function when the page loads.
 window.addEventListener('load', setUpTouchables, false);
